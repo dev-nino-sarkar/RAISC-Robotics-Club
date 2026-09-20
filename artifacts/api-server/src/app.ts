52d06fe2e8f0f2ff1851f97fda5e6350944a5682
dev-nino-sarkar <ironrishav@gmail.com>
2026-09-20 13:23:00 +0530

    fixed even more changes

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,6 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp, { type HttpLogger } from "pino-http";
+import { pinoHttp } from "pino-http";
 import type { IncomingMessage, ServerResponse } from "http";
 import router from "./routes";
 import { logger } from "./lib/logger";
@@ -8,7 +8,7 @@ import { logger } from "./lib/logger";
 const app: Express = express();
 
 app.use(
-  (pinoHttp as unknown as (opts: object) => HttpLogger)({
+  pinoHttp({
     logger,
     serializers: {
       req(req: IncomingMessage & { id?: string }) {
